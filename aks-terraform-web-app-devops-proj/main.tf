39f76df7e7a780f1cbc61572911bed3ff03ea806
afzalm <afzal_maruf@yahoo.co.uk>
2023-12-30 15:25:47 +0000

    Create directory 'aks-terraform-web-app-devops-proj' and basic 'main.tf' file for Milestone 4, Task 1.

---
+++ aks-terraform-web-app-devops-proj/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  client_id       = ""
+  client_secret   = ""
+  subscription_id = ""
+  tenant_id       = ""
+  skip_provider_registration = "true"
+}
