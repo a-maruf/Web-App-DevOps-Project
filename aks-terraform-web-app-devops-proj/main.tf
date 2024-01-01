46dee48da14e1a1ab1ccc358e56f1b8a9565f163
afzalm <afzal_maruf@yahoo.co.uk>
2024-01-01 17:11:59 +0000

    Update 'aks-terraform-web-app-devops-proj/main.tf' with some module details

--- aks-terraform-web-app-devops-proj/main.tf
+++ aks-terraform-web-app-devops-proj/main.tf
@@ -1,4 +1,4 @@
-# Web-App-DevOps-Project/main.tf
+# aks-terraform-web-app-devops-proj/main.tf
 
 terraform {
   required_providers {
@@ -16,4 +16,27 @@ provider "azurerm" {
   subscription_id = ""
   tenant_id       = ""
   skip_provider_registration = "true"
+}
+
+module "networking" {
+  source = "./networking-module"
+  
+  resource_group_name = "your_resource_group_name"
+  location            = "your_location"
+  vnet_address_space  = ["10.0.0.0/16"]
+}
+
+module "aks_cluster" {
+  source = "./aks-cluster-module"
+
+  aks_cluster_name           = "your_aks_cluster_name"
+  cluster_location           = "your_cluster_location"
+  dns_prefix                 = "your_dns_prefix"
+  kubernetes_version         = "your_kubernetes_version"
+  service_principal_client_id = "your_service_principal_client_id"
+  service_principal_secret    = "your_service_principal_secret"
+  resource_group_name         = module.networking.networking_resource_group_name
+  vnet_id                     = module.networking.vnet_id
+  control_plane_subnet_id     = module.networking.control_plane_subnet_id
+  worker_node_subnet_id       = module.networking.worker_node_subnet_id
 }
